327e120ab4236e5e3138db8a93d3dd40e721f6fb
Sanil <web.developer.sanil@gmail.com>
2018-06-22 05:35:54 +0000

    controller

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,10 @@ Route::get('/about', function () {
 });
 
 /**
- * about
- */
-Route::get('/contact', function () {
-    return print_r($_REQUEST, true);
-});
-
-/**
  * Route with parameter
  */
-Route::get('/post/{id}', function ($id) {
-    return "This is post : $id";
+Route::get('/contact/{id}', function ($id) {
+    return "This is contact : $id";
 });
 
 /**
@@ -46,17 +39,29 @@ Route::get('/category/{id}/userId/{userId}', function ($id, $userId) {
     return "This is category : $id, for $userId";
 });
 
-
 /**
-* nickname or alias for route
-* from console, you can check route using $ php artisan route:list
-*/
+ * nickname or alias for route
+ * from console, you can check route using $ php artisan route:list
+ */
 
-Route::get('/admin/login', ['as'=>'admin.login', function(){
-	
-	$url = route('admin.login');
+Route::get('/admin/login', ['as' => 'admin.login', function () {
 
-	return "this url is $url";
+    $url = route('admin.login');
+
+    return "this url is $url";
 }]);
 
+
+
+/**
+ * route to controller
+ */
+
+Route::get('/test', 'TestController@index');
+Route::get('/test/{id}', 'TestController@show');
+
+/**
+* Route to controller resource...
+*/
+Route::resource('post', 'PostController');
 
